fa7aca29810cf8a94de89352a786273f396863e1
Minaam Ahmed Awan <minaam98@gmail.com>
2021-06-30 13:37:27 +0500

    Initial Commit

---
+++ Live OCR Text Recognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live OCR Text Recognition
+//
+//  Created by Minaam Ahmed Awan on 30/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
